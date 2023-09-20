bd2124ad7d37d1026f10d53ab95e9d545415113d
hermes7144 <kichancho@gmail.com>
2023-09-20 18:00:12 +0900

    feat:1.라우터작업2.네비바 작업중

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <header>
+      <Link to='/'>동네책방</Link>
+      <button>33</button>
+    </header>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+import Allbooks from './pages/Allbooks';
+import NewBook from './pages/NewBook';
+import Neighborhood from './pages/Neighborhood';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: '/', element: <Home /> },
+      { path: '/books', element: <Allbooks /> },
+      {
+        path: '/books/new',
+        element: <NewBook />,
+      },
+      {
+        path: '/neighborhood',
+        element: <Neighborhood />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
@@ -17,3 +41,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+

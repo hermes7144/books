464257b7c243abb69e3359df69f2bf723a704577
hermes7144 <kichancho@gmail.com>
2023-09-21 17:56:08 +0900

    feat:firebase 셋팅중

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { BsFillPencilFill } from 'react-icons/bs';
 export default function Navbar() {
   return (
-    <header>
-      <Link to='/'>동네책방</Link>
-      <button>33</button>
+    <header className='flex justify-between border-b border-gray-300 p-2'>
+      <nav className='flex items-end gap-4 font-semibold'>
+        <Link to='/' className='text-4xl text-primary'>
+          동네책방
+        </Link>
+        <Link to='/neighborhood'>동네인증</Link>
+      </nav>
+      <nav className='flex items-center gap-4 font-semibold'>
+        <Link to='/books/new' className='text-xl'>
+          <BsFillPencilFill />
+        </Link>
+        <button>Login</button>
+      </nav>
     </header>
   );
 }

09490b1104ab9768a1d121d9585da5dd58e0512a
hermes7144 <kichancho@gmail.com>
2023-11-14 11:09:34 +0900

    feat:토스트 위치 조정

--- src/components/ui/Toast.tsx
+++ src/components/ui/Toast.tsx
@@ -14,5 +14,5 @@ export default function Toast({ message, onClose }) {
     };
   }, []);
 
-  return <div className={`fixed bottom-4 left-1/2 bg-gray-800 text-white px-4 py-2 rounded-md transition-opacity duration-300 ${visible ? 'opacity-100' : 'opacity-0'}`}>{message}</div>;
+  return <div className={`fixed left-36 md:left-1/2 bottom-1/2 bg-gray-800 text-white px-4 py-2 rounded-md transition-opacity duration-300 ${visible ? 'opacity-100' : 'opacity-0'}`}>{message}</div>;
 }
